cc6ae241eae7aca302074db19e7736334a4b0aab
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-09-25 09:25:14 +0200

    första delen klar

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner tb = new Scanner (System.in);
+        int [] fält1 = new int [4];
+        System.out.println("Droppa lite tal");
+        fält1[0] = tb.nextInt();
+        fält1[1] = tb.nextInt();
+        fält1[2] = tb.nextInt();
+        fält1[3] = tb.nextInt();
+        int middle = fält1[0];
+        fält1[0] = fält1[3];
+        fält1[3] = middle;
+        middle = fält1[1];
+        fält1[1] = fält1[2];
+        fält1[2] = middle;
+
+        
+
+    }
+}
