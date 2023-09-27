06f11a06085c1cbf8cd9c93b747c211799e158a9
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-09-27 10:29:56 +0200

    Idk klar ig

--- src/App.java
+++ src/App.java
@@ -9,6 +9,7 @@ public class App {
         fält1[1] = tb.nextInt();
         fält1[2] = tb.nextInt();
         fält1[3] = tb.nextInt();
+
         int middle = fält1[0];
         fält1[0] = fält1[3];
         fält1[3] = middle;
@@ -18,6 +19,33 @@ public class App {
 
         System.out.println("Finit, Nexxt progratime");
         
+        String []  fält2 = new String [3];
+        Scanner scan = new Scanner (System.in);
+        fält2[0] = "Mina";
+        fält2[1] = "Sjungande";
+        fält2[2] = "Monster";
 
+        System.out.println("Gissa på ett ord jag har skrivit tehee");
+        String gissning = scan.nextLine();
+        
+        boolean svar = (fält2[0].equalsIgnoreCase(gissning));
+        if (svar == true){
+            System.out.println("wtf hax?");
+        }
+        else{
+            svar = (fält2[1].equalsIgnoreCase(gissning));
+            if(svar == true){
+                System.out.println("wtf hax?");
+            }
+            else{
+                svar = (fält2[2].equalsIgnoreCase(gissning));
+                if(svar == true){
+                System.out.println("wtf hax?");
+              }
+              else{
+                System.out.println("noob");
+              }
+            }
+        }
     }
 }
